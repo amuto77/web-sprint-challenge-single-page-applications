5614ee1d11eb6992f8cee1d1a0be72db9610413f
Andrew Muto <amuto77@gmail.com>
2020-07-24 13:32:16 -0400

    Completed index, home, navigation, and parts of app and pizza

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom"
+import Navigation from "./components/Navigation"
+import Pizza from "./components/Pizza"
+import Completion from "./components/Completion"
+import Home from "./components/Home"
+import "./App.css";
 
-const App = () => {
+const initialForm = {
+  name:'',
+  size:'',
+  pepperoni: false,
+  onions: false,
+  spinach: false,
+  mushrooms: false,
+  instructions: ''
+}
+
+function App() {
+  const [form, setForm] = useState();
   return (
-    <>
+    <div className="App">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <Navigation />
+      <Switch>
+        <Route path ="/pizza">
+          <Pizza />
+        </Route>
+      <Route path="/complete">
+        <Completion />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>    
+    </div>
+
   );
 };
 export default App;
